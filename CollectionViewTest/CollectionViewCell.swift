5691d73bb3a809040491656d7f25f6c6fee6b755
Gregorius Albert <galbert.djaja@gmail.com>
2022-04-27 11:29:36 +0700

    Custom Cell

---
+++ CollectionViewTest/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewTest
+//
+//  Created by Gregorius Albert on 27/04/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+}
--- CollectionViewTest/ViewController.swift
+++ CollectionViewTest/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let arrContents = ["Alpha", "Beta", "Charlie", "Delta", "Eugene", "Franklin"]
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.textLabel.text = arrContents[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrContents.count
     }
 
 
